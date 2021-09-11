c4e62f173b9ca73c8568e9a7772aac15b2bfb5da
umessh <umesh.gangundi@gmail.com>
2021-09-11 15:35:24 +1200

    AWS Account id variable

--- variables.tf
+++ variables.tf
@@ -4,3 +4,12 @@ variable "aws_region" {
   default = "ap-southeast-2"
 }
 
+variable "lambda_logstream_name" {
+  description = "AWS cloudwatch log stream name"
+  default = "test"
+}
+
+variable "aws_acc_id" {
+  description = "AWS account id"
+  default = "<id>"
+}
